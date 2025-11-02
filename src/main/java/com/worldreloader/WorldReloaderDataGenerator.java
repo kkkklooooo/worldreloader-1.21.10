47c13e34b68f6d56462529b3be650eca204b2900
kkkklooooo <mabo27158@gmail.com>
2025-11-02 10:47:10 +0800

    First

---
+++ src/main/java/com/worldreloader/WorldReloaderDataGenerator.java
@@ -0,0 +1,11 @@
+package com.worldreloader;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class WorldReloaderDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
